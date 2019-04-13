dd9fc0bf02dc97b245b0932a1b608547fe41297f
Ayush Dobhal <adobhal93@gmail.com>
2019-04-13 12:44:38 -0500

    Updated stream producer

--- stream_producer.py
+++ stream_producer.py
@@ -1,29 +1,51 @@
-# -*- coding: utf-8 -*-
-
 from kafka import KafkaProducer
 from time import sleep
 import json, sys
 import requests
 import time
+import os
+import re
                   
 def getData(url):
     jsonData = requests.get(url).json()
     data = []
     labels = {}
     index = 0
+    id = 0
 
+    if os.path.exists("./labels.csv"):
+        file = open("labels.csv")
+        for line in file:
+            splitArr = line.split(",")
+            labels[splitArr[0]] = int(splitArr[1])
+            index += 1
+        file.close()
+    
     for i in range(len(jsonData["response"]['results'])):
         headline = jsonData["response"]['results'][i]['fields']['headline']
         bodyText = jsonData["response"]['results'][i]['fields']['bodyText']
-        #headline += bodyText
+        trailText = jsonData["response"]['results'][i]['fields']['trailText']
         label = jsonData["response"]['results'][i]['sectionName']
         if label not in labels:
+            continue
             labels[label] = index
             index += 1
-        #data.append({'label':labels[label],'Descript':headline})
-        toAdd = headline+'|'+bodyText+'|'+str(labels[label])+'|'+label
-        #print("csv = "+toAdd)
-        data.append(toAdd)
+		
+        headline = re.sub(r"[^a-zA-Z0-9]+", ' ', headline)
+        bodyText = re.sub(r"[^a-zA-Z0-9]+", ' ', bodyText)
+        trailText = re.sub(r"[^a-zA-Z0-9]+", ' ', trailText)
+        headline = re.sub(r'[\n\r]+', '', headline)
+        bodyText = re.sub(r'[\n\r]+', '', bodyText)
+        toAdd = str(id) + '|'+headline+' '+bodyText+' '+trailText+'|'+str(labels[label])
+        id += 1
+        toAdd.replace("\r", "\t")
+        data.append(toAdd.strip())
+
+    file = open("labels.csv", "w")
+    for key, value in labels.items():
+        file.write(key+","+str(value)+"\n")
+    file.close()
+        
     return(data)
 
 def publish_message(producer_instance, topic_name, value):
@@ -40,8 +62,7 @@ def publish_message(producer_instance, topic_name, value):
 def connect_kafka_producer():
     _producer = None
     try:
-        #_producer = KafkaProducer(value_serializer=lambda v:json.dumps(v).encode('utf-8'),bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
-         _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
+        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
     
     except Exception as ex:
         print('Exception while connecting Kafka')
@@ -50,31 +71,17 @@ def connect_kafka_producer():
         return _producer
 
 if __name__== "__main__":
-    
-    if len(sys.argv) != 4: 
-        print ('Number of arguments is not correct')
-        #exit()
-    
-    #key = sys.argv[1]
     key = '7faccc33-5d6a-4300-9e0b-1e5aa723f6e7'
-    #fromDate = sys.argv[2]
-    #toDate = sys.argv[3]
-    fromDate = '2018-10-1'
-    toDate = '2018-10-31'
+    fromDate = '2019-01-1'
+    toDate = '2019-03-3'
     
     url = 'http://content.guardianapis.com/search?from-date='+ fromDate +'&to-date='+ toDate +'&order-by=newest&show-fields=all&page-size=200&%20num_per_section=10000&api-key='+key        
-    all_news=getData(url)
-    f = open("trainingdata.csv", "a")
+    all_news = getData(url)
+	
     if len(all_news)>0:
-        prod=connect_kafka_producer();
+        prod = connect_kafka_producer();
         for story in all_news:
-            print(json.dumps(story))
-            f.write(json.dumps(story))
-            #f.close()
-            #publish_message(prod, 'test', story)
-            #exit()
-            #time.sleep(1)
+            publish_message(prod, 'test', story)
+            time.sleep(1)
         if prod is not None:
                 prod.close()
-
-    f.close()
